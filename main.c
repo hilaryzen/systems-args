a6a251d586e3a1cbe85af6acd1b9635d62f882aa
Hilary Zen <hzen00@stuy.edu>
2019-11-27 03:24:55 -0500

    Debugging

--- main.c
+++ main.c
@@ -3,21 +3,41 @@
 #include <string.h>
 #include <unistd.h>
 
+char **parse_args(char *line);
+
 char **parse_args(char *line) {
-  char **args;
-  char *s;
+  char **args = malloc(5 * sizeof(char *));
+  char *s = line;
+  /*
   s = strsep(&line, " ");
-  printf("%s", s);
+  printf("%s\n", s);
+  printf("%s\n", line);
+  */
+  int i = 0;
+  while (*s != '\0') {
+    args[i] = strsep(&s, " ");
+    printf("%s\n", args[i]);
+    i++;
+  }
+  args[i] = NULL;
   return args;
 }
 
 int main() {
-  char path[100];
+  char line[100];
   printf("Enter a command: ");
-  fgets(path, 100, stdin);
-  if (strchr(path, '\n') != NULL) {
-    *(strchr(path, '\n')) = '\0';
+  fgets(line, 100, stdin);
+  /*
+  if (strchr(line, '\n') != NULL) {
+    *(strchr(line, '\n')) = '\0';
   }
+  */
+  printf("Starting\n");
+  char ** args = parse_args(line);
+  printf("Executing\n");
+  execvp(args[0], args);
+  printf("Freeing\n");
+  free(args);
 
   return 0;
 }
